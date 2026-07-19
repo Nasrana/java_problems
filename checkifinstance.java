48871e8b7029a595d88d5e337767ed26e90c93c1
Nasrana Fathima S.A <ff391048@gmail.com>
2026-07-19 21:23:26 +0530

    Add files via upload

---
+++ checkifinstance.java
@@ -0,0 +1,25 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (obj === null || obj === undefined || typeof classFunction !== 'function') {
+        return false;
+    }
+    
+    // Wrap primitives so we can access their prototype chain
+    Object(obj);
+    obj = Object(obj);
+    
+    let proto = Object.getPrototypeOf(obj);
+    
+    while (proto !== null) {
+        if (proto === classFunction.prototype) {
+            return true;
+        }
+        proto = Object.getPrototypeOf(proto);
+    }
+    
+    return false;
+};
