1d9ef33cdca9aede01fa2a01c697760db3e4dec4
Nasrana Fathima S.A <ff391048@gmail.com>
2026-07-20 11:01:25 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,19 @@
+class length {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces from the end
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count characters of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
