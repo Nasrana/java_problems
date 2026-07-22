c41aeab8d6674dec2be6aa0ca0dbfa50476a59ca
Nasrana Fathima S.A <ff391048@gmail.com>
2026-07-22 10:49:26 +0530

    Add files via upload

---
+++ Tolower.java
@@ -0,0 +1,12 @@
+class Tolower {
+    public String toLowerCase(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                result.append((char) (c + 32));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
